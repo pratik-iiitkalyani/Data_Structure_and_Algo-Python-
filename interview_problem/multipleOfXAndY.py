5401cb5e8ad8fdfaba027e41375f5e90db2ae8bc
Pratik Kumar <pkraja121dss@gmail.com>
2019-11-15 12:24:50 +0530

    Update multipleOfXAndY.py

--- interview_problem/multipleOfXAndY.py
+++ interview_problem/multipleOfXAndY.py
@@ -1,38 +1,22 @@
 # write a code to find out all positive whole
 #number x and Y such that x*y = 130
 
-def func():
+def multiply():
     i = 1
+    x = y = 0
     n = 130
-    x = 0
-    y = 0
-    while i*n == 130:
+    while i*n == 130 and i < 11:
         x += i
         y += n
-        # if i<=26:
-        print("{} and {}".format(x, y))
+        print(f"{x} and {y}")
+        x = 0
+        y = 0
         if (i%2 == 0): 
             i += 3
             n = int(130/i)
         elif (i%2 != 0):
             i *= 2
             n = int(130/i)
-        # elif (i%2 == 0 and i>= 26):
-        #     i += 39
-        #     n = int(130/i)
-        x = 0
-        y = 0
-        # else:
-        #     print(i)
-        #     x = 0
-        #     y = 0
-        #     if (i%2 == 0): 
-        #         i += 39
-        #         n = int(130/i)
-        #         print("{} and {}".format(x, y))
-        #     elif (i%2 != 0):
-        #         i *= 2
-        #         n = int(130/i)
-        #         print("{} and {}".format(x, y))
-
-func()
+    
+multiply()
+        
