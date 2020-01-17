5fa898ae62a8e929d6eb9d38f16f6277a22838a1
Kumar <ts-pratik.kumar@rakuten.com>
2020-01-17 19:10:07 +0530

    stack_intro

--- stack/stack_intro.py
+++ stack/stack_intro.py
@@ -1,28 +1,37 @@
 class Stack:
     def __init__(self):
         self.items = []
-    
-    def push(self, item):
-        self.items.append(item)
-    
-    def pop(self):
-        return self.items.pop()
-
-    def isEmpty(self):
-        return self.items == []
-    
-    def top(self):
-        return self.items[-1]
 
     def get_stack(self):
         return self.items
-    
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return "stack is empty"
+
+    def is_empty(self):
+        return self.items == []
+
+    def top(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return "stack is empty"
+
 s = Stack()
-s.push(10)
-s.push(20)
-print(s.get_stack())
+
+print(s.top())
+print(s.top())
 s.push("a")
 s.push("b")
 print(s.get_stack())
-s.pop()
+s.push(10)
+s.push(20)
+print(s.pop())
 print(s.top())
+print(s.get_stack())
+
+    
